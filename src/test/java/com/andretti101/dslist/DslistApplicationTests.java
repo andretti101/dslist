f5ce0d1a5c9a29dd484c714e5ed0fa1a640d512c
andretti101 <enzoandretti3@gmail.com>
2026-05-01 16:43:34 -0300

    Project created

---
+++ src/test/java/com/andretti101/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andretti101.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
