c43c16115020c22e858eacfa15e4ffb491faa4b8
andretti101 <enzoandretti3@gmail.com>
2026-05-03 21:36:40 -0300

    GET games by list id

--- src/main/java/com/andretti101/dslist/services/GameService.java
+++ src/main/java/com/andretti101/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.andretti101.dslist.services;
 import com.andretti101.dslist.dto.GameDTO;
 import com.andretti101.dslist.dto.GameMinDTO;
 import com.andretti101.dslist.entities.Game;
+import com.andretti101.dslist.projections.GameMinProjection;
 import com.andretti101.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List <Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List <GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
